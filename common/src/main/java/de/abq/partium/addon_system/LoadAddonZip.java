6ee1b2ae01c2c79afc8ec6722d33571d67d7b2c9
Cron3x <cron3x.exe@gmail.com>
2025-04-13 10:57:44 +0200

    clean start

--- common/src/main/java/de/abq/partium/addon_system/LoadAddonZip.java
+++
@@ -1,9 +0,0 @@
-package de.abq.partium.addon_system;
-
-import net.minecraft.server.MinecraftServer;
-
-public class LoadAddonZip {
-    public void loadAddon(MinecraftServer server){
-
-    }
-}
--- common/src/main/java/de/abq/partium/addon_system/json_objects/PackObject.java
+++
@@ -1,7 +0,0 @@
-package de.abq.partium.addon_system.json_objects;
-
-public class PackObject {
-    public String id;
-    public String author;
-    public String url;
-}
